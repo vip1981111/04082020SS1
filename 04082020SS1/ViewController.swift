721f2f1930708e5bba5658f6ac35d377f12558f4
mohammed <vip1981.1@gmail.com>
2020-08-04 01:39:20 +0300

    Initial Commit

---
+++ 04082020SS1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  04082020SS1
+//
+//  Created by Mohammed Abdullah on 14/12/1441 AH.
+//  Copyright © 1441 Mohammed Abdullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
